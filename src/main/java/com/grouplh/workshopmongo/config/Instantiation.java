13834d0edcea11c2df923e1b778641edd9718c98
LuizHenryBR <siqueiralh7768@hotmail.com>
2024-06-11 11:03:10 -0300

    Projection of author data using DTO

--- src/main/java/com/grouplh/workshopmongo/config/Instantiation.java
+++ src/main/java/com/grouplh/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.grouplh.workshopmongo.domain.Post;
 import com.grouplh.workshopmongo.domain.User;
+import com.grouplh.workshopmongo.dto.AuthorDTO;
 import com.grouplh.workshopmongo.repository.PostRepository;
 import com.grouplh.workshopmongo.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		userRepository.saveAll(Arrays.asList(maria,alex,bob)); // A user need to exist (be saved) before being associated at the Post
 		
-		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria)); // Using AuthorDTO to show only the info I want
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
