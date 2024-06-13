7565e505df0ab9022199cef6cf129b5b2a8c8d34
LuizHenryBR <siqueiralh7768@hotmail.com>
2024-06-13 10:32:06 -0300

    Using @Query to do a simple query

--- src/main/java/com/grouplh/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/grouplh/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.grouplh.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.grouplh.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{ //MongoRepository<> provides all the necessary methods which help to create a CRUD application and it also supports the custom derived query methods
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }") //https://www.mongodb.com/docs/manual/reference/operator/query/regex/
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text); //Spring Data Query Method, do auto generated consult. https://docs.spring.io/spring-data/mongodb/reference/mongodb/repositories/query-methods.html
 
