ae6c375ab0e5d112c4ac15104ff0d6ac27a2df90
LuizHenryBR <siqueiralh7768@hotmail.com>
2024-06-13 10:22:40 -0300

    Simple query using MongoDB-specific Query Methods

--- src/main/java/com/grouplh/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/grouplh/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.grouplh.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,6 @@ import com.grouplh.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{ //MongoRepository<> provides all the necessary methods which help to create a CRUD application and it also supports the custom derived query methods
 	
-	
+	List<Post> findByTitleContainingIgnoreCase(String text); //Spring Data Query Method, do auto generated consult. https://docs.spring.io/spring-data/mongodb/reference/mongodb/repositories/query-methods.html
 
 }
